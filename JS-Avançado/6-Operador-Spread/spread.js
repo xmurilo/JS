4b1ddcfc4f7b920685aadc93360281849bb6eef6
Murilo Silva <dixmurilox@gmail.com>
2023-06-26 15:20:05 -0300

    operador spread

---
+++ JS-Avançado/6-Operador-Spread/spread.js
@@ -0,0 +1,13 @@
+// const pessoas = ['Joao', 'Natan', 'Maria'];
+// console.log(...pessoas);
+
+// CONCATENAR DOIS ARRAYS
+// let pessoas1 = ['Joao', 'Natan', 'Maria'];
+// let pessoas2 = ['Ana', 'Gabriel', 'Caio'];
+// pessoas1 = [...pessoas1, ...pessoas2];
+// console.log(pessoas1);
+
+// CLONAR OBJETO
+const pessoa1 = { nome: 'Mari', idade: 23 };
+let objetoClonado = { ...pessoa1 };
+console.log(objetoClonado);
